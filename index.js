6b45d8101a870cd96991c8b92b16fbd819761b9e
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2024-09-14 22:55:47 +0200

    CLient react
    Ajout d'un projet react dans le projet node.js afin de configurer les interfaces clients.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -8,4 +9,17 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
+});
+
+// Servir les fichiers statiques du build de React
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Toutes les autres requêtes renverront le fichier index.html de React
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
